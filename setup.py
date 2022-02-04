7fa293b70ae4442e409aa4b011cf1b2b3779e4ce
Henrik Hviid Hansen <HEHHA@orsted.dk>
2022-02-04 17:55:34 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version="0.9.0",
     description="Python package with selected SPC functions",
     author="Henrik Hviid Hansen",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
     packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"]
 )
